792541f32b02967587eb03e8f80524813b6c9e6c
Randolph Higgins <higginsrandolph78@gmail.com>
2015-03-06 21:44:25 -0500

    edited stuff

--- 06quickSelect/quickSelect.java
+++ 06quickSelect/quickSelect.java
@@ -2,65 +2,47 @@ import java.util.*;
 public class quickSelect{
     public static void main(String[]args){
 	Random r = new Random();
-	int[]ar = new int[10];
+	int[]ar = new int[10000];
 	int i = 0;
 	while(i < ar.length){
-	    ar[i] = r.nextInt(200);
+	    ar[i] = r.nextInt(2000)-1000;
 	    i++;
 	}
-	System.out.println(Arrays.toString(ar));
-	ar = pivot(ar,0,10);
-	System.out.println(Arrays.toString(ar));
+	System.out.println(check(ar));
+	ar = pivot(ar, 0, ar.length, 5);
+	System.out.println(check(ar));
 	//Arrays.sort(ar);
 	//System.out.println(Arrays.toString(ar));
 	//System.out.println(ar[4]);
     }
-
-    /*
-    public static int pivotFind(int[]ary, int find){
-	int start = 0;
-	int end = ary.length;
-	int now = 1;
-	while(find != now){
-	    System.out.println(Arrays.toString(ary)+"\n"+start+":"+end);
-	    now = partition(ary, start, end);
-	    if(now > find){
-		end = now;
-	    }
-	    if(now < find){
-		start = now;
-	    }
-	    System.out.println(Arrays.toString(ary));
-	}
-	return ary[now];
-    }
-    */
-    public static int[] pivot(int[]ary, int Si, int Ei){
+    public static void partition(int[]ary,int Si, int Ei){
 	Random r = new Random();
+	int pivot = r.nextInt(Ei-Si) + Si;
 	int[] D = new int[ary.length];
 	for(int i = 0; i < ary.length; i++){
-	    if(i < Si && i >= Ei){
+	    if(i < Si || i >= Ei){
 		D[i] = ary[i];
 	    }
 	}
-	int pivot = Si + r.nextInt(Ei-Si);
+	//System.out.println(Arrays.toString(ary)+ "<<<");
+	//int pivot = Si + r.nextInt(Ei-Si);
 	int start = Si;
 	int end = Ei - 1;
 	for(int i = Si; i < Ei; i++){
-	    System.out.println(Arrays.toString(D));
-	    if(ary[i] != ary[pivot]){
+	    //  System.out.println(Arrays.toString(D));
+	    if(i != pivot){
 		if(ary[i] < ary[pivot]){
 		    D[start]=ary[i];
 		    start++;
 		}else{
-		    D[end]=ary[i];
-		    end--;
-		}
-	    }else{
-		if(i != pivot){
-		    if(i%2 == 0){
-			D[start]=ary[i];
-			start++;
+		    if(ary[i] == ary[pivot]){
+			if(i%2 == 1){
+			    D[start]=ary[i];
+			    start++;
+			}else{
+			    D[end]=ary[i];
+			    end--;
+			}
 		    }else{
 			D[end]=ary[i];
 			end--;
@@ -69,7 +51,57 @@ public class quickSelect{
 	    }
 	}
 	D[start] = ary[pivot];
-	ary = D;
-	System.out.println(Arrays.toString(ary));
+    }
+    public static boolean check(int[] ar){
+	for(int i = 0; i < ar.length - 1; i++){
+	    if(ar[i] - ar[i+1] > 0){
+		return false;
+	    }
+	}
+	return true;
+    }
+    public static int[] quickSort(int[]ar){
+	return pivot(ar, 0, ar.length, ar.length/2);
+    }
+    public static int[] pivot(int[]ary, int Si, int Ei, int pivot){
+	if(Ei-Si <= 1)
+	    return ary;
+	Random r = new Random();
+	int[] D = new int[ary.length];
+	for(int i = 0; i < ary.length; i++){
+	    if(i < Si || i >= Ei){
+		D[i] = ary[i];
+	    }
+	}
+	//System.out.println(Arrays.toString(ary)+ "<<<");
+	//int pivot = Si + r.nextInt(Ei-Si);
+	int start = Si;
+	int end = Ei - 1;
+	for(int i = Si; i < Ei; i++){
+	    //  System.out.println(Arrays.toString(D));
+	    if(i != pivot){
+		if(ary[i] < ary[pivot]){
+		    D[start]=ary[i];
+		    start++;
+		}else{
+		    if(ary[i] == ary[pivot]){
+			if(i%2 == 1){
+			    D[start]=ary[i];
+			    start++;
+			}else{
+			    D[end]=ary[i];
+			    end--;
+			}
+		    }else{
+			D[end]=ary[i];
+			end--;
+		    }
+		}
+	    }
+	}
+	D[start] = ary[pivot];
+	D = pivot(D, Si, end, (start+Si)/2);
+	D = pivot(D, start, Ei, r.nextInt(Ei-end)+end);
+	return D;
     }
 }
