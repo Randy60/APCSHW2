011ff47d846b82b658455c2d0b29638e989ea07c
Randolph Higgins <higginsrandolph78@gmail.com>
2015-03-11 16:07:25 -0400

    basic LNodes

---
+++ 07LNode/LNode.java
@@ -0,0 +1,37 @@
+public class LNode{
+    private Object value;
+    private LNode next;
+    public LNode(Object n){
+	value = n;
+    }
+    public void remove(int n){
+	if(n <= 1){
+	    next = next.next;
+	}else{
+	    next.remove(n-1);
+	}
+    }
+    public void add(int n, Object c){
+	if(n <= 1){
+	    LNode r = new LNode(c);
+	    r.next = next;
+	    next = r;
+	}else{
+	    next.add(--n, c);
+	}
+    }
+    public Object get(int n){
+	if(n == 0){
+	    return value;
+	}else{
+	    return next.get(--n);
+	}
+    }
+    public void set(int n, Object o){
+	if(n == 0){
+	    value = o;
+	}else{
+	    next.set(--n, o);
+	}
+    }
+}
