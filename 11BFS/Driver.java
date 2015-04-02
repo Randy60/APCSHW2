9b28ee27f73def035cdba8ee9ce568c002d49e79
Randolph Higgins <higginsrandolph78@gmail.com>
2015-04-01 21:36:30 -0400

    praying this works

---
+++ 11BFS/Driver.java
@@ -0,0 +1,13 @@
+public class Driver{
+    public static void main(String[]args){
+	String s = "*******"+"\n";
+	        s+="*S    *"+"\n";
+		s+="*  ** *"+"\n";
+		s+="*   * *"+"\n";
+		s+="*   ***"+"\n";
+		s+="*    E*"+"\n";
+		s+="*******"+"\n";
+		Maze prayer = new Maze(s);
+    }
+}
+				
--- 11BFS/Maze.java
+++ 11BFS/Maze.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 public class Maze{
     final static String clear =  "\033[2J";
     public void wait(int millis){
@@ -8,23 +9,57 @@ public class Maze{
 				catch (InterruptedException e) {
 				}
     }
+    private int maxx, maxy;
+    public int[] start = new int[2];
     public Maze(String filename){
-    
+	//read the whole maze into a single string first
+	String ans = "";
+	try{
+	    Scanner in = new Scanner(new File(filename));
+
+	    //keep reading next line
+	    while(in.hasNext()){
+		String line = in.nextLine();
+		if(maxy == 0){
+		    //calculate width of the maze
+		    maxx = line.length();
+		}
+		//every new line add 1 to the height of the maze
+		maxy++;
+		ans += line;
+	    }
+	}
+	catch(Exception e){
+	    System.out.println("File: " + filename + " could not be opened.");
+	    e.printStackTrace();
+	    System.exit(0);
+	}
+
+	//copy from the single string to a 2D array
+	Maze = new char[maxx][maxy];
+	for(int i = 0; i < ans.length(); i++){
+	    char c = ans.charAt(i);
+	    Maze[i % maxx][i / maxx] = c;
+	    if(c == 'S'){
+		start[1] = i % maxx;
+		start[0] = i / maxx;
+	    }
+	}
     }
-    public char[][] maze;
+    public char[][] Maze;
     public String toString(){
 	String s = "";
-	for(int i = 0; i < maze[0].length; i++){
+	for(int i = 0; i < Maze[0].length; i++){
 	    s+="%";
 	}
-	for(int i = 0; i < maze.length; i++){
+	for(int i = 0; i < Maze.length; i++){
 	    s+="\n"+"%";
-	    for(int j = 0; j < maze[i].length; j++){
-		s+=maze[i][j];
+	    for(int j = 0; j < Maze[i].length; j++){
+		s+=Maze[i][j];
 	    }
 	    s+="%";
 	}
-	for(int i = 0; i < maze[0].length; i++){
+	for(int i = 0; i < Maze[0].length; i++){
 	    s+="%";
 	}
 	return s;
@@ -39,17 +74,42 @@ public class Maze{
 	    }else{
 		next = Frontier.removeLast();
 	    }
-	    if(Maze[next[1]][next[0]] != '*'){
+	    if(Maze[next[0]][next[1]] != '*' && Maze[next[0]][next[1]] != '-'){
 		if(animate){
 		    wait(20);
-		    System.out.println(clear+toString);
+		    System.out.println(clear);
+		    System.out.println(toString());
 		}
-		if(Maze[next[1]][next[0]] == 'E'){
+		if(Maze[next[0]][next[1]] == 'E'){
+		    for(int i = 0; i < Maze.length*Maze[0].length; i++){
+			if(Maze[i/Maze.length][i%Maze.length] == '-')
+			    Maze[i/Maze.length][i%Maze.length] = ' ';
+		    }
+		    for(int i = 2; i < Maze.length/2 - 1; i++){
+			Maze[next[i]][next[++i]] = '$';
+			wait(100);
+			System.out.println(clear+toString());
+		    }
 		    return true;
 		}
+		Maze[next[0]][next[1]] = '-';
+		int[] adder = new int[next.length+2];
+		for(int i = 2; i < adder.length; i++){
+		    adder[i] = next[i-2];
+		}
+		adder[0] = next[0]+1;
+		adder[1] = next[1]+1;
+		Frontier.addLast(adder);
+		adder[0]-=2;
+		Frontier.addLast(adder);
+		adder[1]-=2;
+		Frontier.addLast(adder);
+		adder[0]+=2;
+		Frontier.addLast(adder);
 	    }
+	}
     }
-
+}
     /**Solve the maze using a frontier in a DFS manner. 
      * When animate is true, print the board at each step of the algorithm.
      * Replace spaces with x's as you traverse the maze. 
@@ -58,7 +118,5 @@ public class Maze{
 
     //public boolean solveBFS(){
     //return solveBFS(false);
-    }
 //public boolean solveDFS(){
 //return solveDFS(false);
-}
